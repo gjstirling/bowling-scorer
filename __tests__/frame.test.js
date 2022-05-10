a04e01c4e1856b8f20bf833ed6e197a8aac54d84
Graeme Stirling <graemejstirling@gmail.com>
2022-05-10 12:16:38 +0100

    Green second test

--- __tests__/frame.test.js
+++ __tests__/frame.test.js
@@ -4,7 +4,15 @@ describe("Roll Method", function () {
 
   test('returns a new frame when given a number (roll)', () => {
     var frame = new Frame()
-    expect(frame.addRoll(6)).toEqual([6,0]);
+    expect(frame.addRoll(6)).toEqual([6]);
   });
+
+  test('Adds second roll to a frame', () => {
+    var frame = new Frame()
+    frame.addRoll(6)
+    expect(frame.addRoll(3)).toEqual([6,3]);
+  });
+
+
 
 });
