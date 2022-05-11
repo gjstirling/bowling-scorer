fd82944bb3a917c4fc0ff0632b3498f665863441
Graeme Stirling <graemejstirling@gmail.com>
2022-05-11 12:53:23 +0100

    Frame knows if it is a spare or strike

--- __tests__/scorecard.test.js
+++ __tests__/scorecard.test.js
@@ -25,4 +25,18 @@ describe("score Method", function () {
         expect(scorecard.score()).toEqual(9);
     });
 
+    test('cCan return score with Spare bonus', () => {
+        var scorecard = new Scorecard()
+        var frame = new Frame 
+        frame.addRoll(6);
+        frame.addRoll(4);
+        var frame2 = new Frame
+        frame.addRoll(6);
+        frame.addRoll(0);
+        scorecard.addFrame(frame)
+        scorecard.addFrame(frame2)
+        console.log(scorecard.frames)
+
+    })
+
 })
