2e4d9d98da43fd9f29b4153c5393ad0b837781a7
Graeme Stirling <graemejstirling@gmail.com>
2022-05-11 15:04:12 +0100

    Green bonus test

--- __tests__/scorecard.test.js
+++ __tests__/scorecard.test.js
@@ -1,16 +1,12 @@
 const Scorecard = require("../lib/scorecard");
 const Frame = require('../lib/frame');
-// jest.mock('./frame');
 
 describe("addFrame Method", function () {
     
     test('Can store a frame', () => {
         var scorecard = new Scorecard()
-        var frame = new Frame 
-        frame.addRoll(6);
-        frame.addRoll(3);
-        scorecard.addFrame(frame)
-        expect(scorecard.addFrame(frame)).toEqual(frame);
+        scorecard.addFrame("mock frame")
+        expect(scorecard.addFrame("mock frame")).toEqual("mock frame");
     });
 })
 
@@ -18,25 +14,20 @@ describe("score Method", function () {
 
     test('Returns the total score of one frame', () => {
         var scorecard = new Scorecard()
-        var frame = new Frame 
-        frame.addRoll(6);
-        frame.addRoll(3);
-        scorecard.addFrame(frame)
+        scorecard.addFrame({rolls: [6,3]})
         expect(scorecard.score()).toEqual(9);
     });
 
-    test('cCan return score with Spare bonus', () => {
-        var scorecard = new Scorecard()
-        var frame = new Frame 
-        frame.addRoll(6);
-        frame.addRoll(4);
-        var frame2 = new Frame
-        frame.addRoll(6);
-        frame.addRoll(0);
-        scorecard.addFrame(frame)
-        scorecard.addFrame(frame2)
-        console.log(scorecard.frames)
+})
 
-    })
+describe("Bonus Method", function () {
+
+    test('Adds bonus score when current frame is a spare', () => {
+        var scorecard = new Scorecard()
+        scorecard.addFrame({rolls: [6,4]})
+        scorecard.addFrame({rolls: [6,0]})
+        expect(scorecard.bonus(0)).toEqual(6);
+    });
 
 })
+
